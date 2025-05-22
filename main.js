cd4648ba75ee2892bd06bc8a62ade856edfb9dbe
Devin Easton <deaston00@outlook.com>
2025-05-22 14:10:21 -0400

    JS Logic sort of finished, working on basic CSS

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-  let sketchpad = document.querySelector("#sketchpad");
   let submit = document.querySelector(".submit");
-  let gridSize = document.getElementById('gridSize');
-
   
+  submit.addEventListener('click', ()=>{
+    const sketchpad = document.querySelector("#sketchpad");
+    const size = document.querySelector('#size');
+    const gridSize = Math.pow(Number(size.value), 2);
+    for (let i = 0; i <= gridSize; i++) {
+      const square = document.createElement('div');
+      square.classList = 'grid-square';
+      sketchpad.appendChild(square);
+    }
+  })
 });
 
 

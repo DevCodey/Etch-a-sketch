d29fbabf3069b12c292e611fc1f01680e08093cb
Devin Easton <deaston00@outlook.com>
2025-05-28 09:02:15 -0400

    Logic for generating grid complete

--- main.js
+++ main.js
@@ -4,10 +4,16 @@ document.addEventListener('DOMContentLoaded', ()=>{
   submit.addEventListener('click', ()=>{
     const sketchpad = document.querySelector("#sketchpad");
     const size = document.querySelector('#size');
+    const squareSize = 100 / size.value;
+
     const gridSize = Math.pow(Number(size.value), 2);
-    for (let i = 0; i <= gridSize; i++) {
+
+    for (let i = 0; i < gridSize; i++) {
       const square = document.createElement('div');
       square.classList = 'grid-square';
+      square.style.flexBasis = `${squareSize}%`;
+      // Width to heigh ratio
+      square.style.aspectRatio = '1 / 1';
       sketchpad.appendChild(square);
     }
   })
